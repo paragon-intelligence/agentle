3be6741055a122f75b57863ea4d747fdc56812d0
Arthur Brenno <64020210+arthurbrenno@users.noreply.github.com>
2025-05-10 09:51:37 -0300

    up

--- agentle/prompts/prompt_providers/db_prompt_provider.py
+++ agentle/prompts/prompt_providers/db_prompt_provider.py
@@ -2,5 +2,6 @@ from typing import TYPE_CHECKING
 from agentle.prompts.prompt_providers.prompt_provider import PromptProvider
 
 if TYPE_CHECKING:
-    
+
 class DBPromptProvider(PromptProvider):
+    ...
